c4e749fca01a25e0c97098da6c774e32db4d652a
mwaqasafzal <mwaqas7064@gmail.com>
2020-08-07 15:44:37 +0500

    Added action to get started btn

--- src/components/Home/Cover.js
+++ src/components/Home/Cover.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {AuthContext} from '../../context/AuthContext';
 import Button from 'react-bootstrap/Button'
 export const Cover=props=>{
+    const {showSignupHandler} = useContext(AuthContext);
     return (
         <div className="cover" id="home">
             <h3>Create or take Quiz anywhere and anytime!</h3>
-            <Button variant="outline-custom">Get Started</Button>
+            <Button onClick={showSignupHandler} variant="outline-custom">Get Started</Button>
         </div>
     );
 }
